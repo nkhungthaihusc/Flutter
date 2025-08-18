a8e996c5eac295af5880c3f09c8e9c9a84714647
Doan Ngoc Tran Chau <gum17112005@gmail.com>
2025-08-18 16:15:38 +0700

    chau update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_application_1/main.dart';
 
-import 'package:flutter_application_1/HomePage.dart';
+import 'package:flutter_application_1/screens/HomePage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
